f6082386b8966c5a7c336968d3ab9522b24ba370
hamzaaltaf481 <hamzaaltaf481@gmail.com>
2024-07-26 12:59:36 +0500

    Exection Faster by using Go routines

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
     "fmt"
     "io/ioutil"
 	"time"
+	"sync"
 )
-func Word_Spaces_Counter(s string){
+func Word_Spaces_Counter(s string, wg *sync.WaitGroup){
+	defer wg.Done()
 
 	wordCounts := make(map[string]int)
 	word := ""
@@ -34,7 +36,9 @@ func Word_Spaces_Counter(s string){
 	fmt.Printf("Total Words: %v\nTotal Spaces: %v\n",total_words,spaces)
 
 }
-func Cpsmlt_Counter(s string){
+func Cpsmlt_Counter(s string, wg *sync.WaitGroup){
+	defer wg.Done()
+
 	Cp := 0
 	sm := 0
 	for _,char := range s{
@@ -49,7 +53,8 @@ func Cpsmlt_Counter(s string){
 	fmt.Printf("Capital Letter : %v\nSmall Letter: %v\n",Cp,sm)
 
 }
-func vowels_Counter(s string){
+func vowels_Counter(s string, wg *sync.WaitGroup){
+	wg.Done()
 
 	vowel := 0
 	for _,char := range s{
@@ -76,9 +81,14 @@ func main() {
 
     text := string(fileContent)
 
-    Word_Spaces_Counter(text)
-	Cpsmlt_Counter(text)
-	vowels_Counter(text)
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+    go Word_Spaces_Counter(text, &wg)
+	go Cpsmlt_Counter(text, &wg)
+	go vowels_Counter(text, &wg)
+
+	wg.Wait()
 
 	end := time.Now()
 	Total_time := end.Sub(start)
